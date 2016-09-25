841c72c065807fe03f49088a43269d36727c17cf
fernando-bernardino <tony.bernardino@gmail.com>
2016-09-25 14:03:40 +0100

    Refactoring to use a service

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -2,8 +2,9 @@
   'use strict';
 
   angular.module('ShoppingCart', [])
-    .controller('BuyController', BuyController)
-    .controller('ToBuyController', ToBuyController);
+    .controller('ToBuyShoppingController', ToBuyShoppingController)
+    .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
   ToBuyController.$inject = ['$scope'];
   function BuyController($scope){
@@ -25,6 +26,40 @@
       $scope.boughtBasket.push(toBuy.basket[index]);
       toBuy.basket.splice(index, 1);
     }
-    console.log(toBuy.basket);
   };
+
+  ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyShoppingController(ShoppingListCheckOffService){
+    var controller = this;
+
+    controller.basket = ShoppingListCheckOffService.toBuy;
+    controller.buy = ShoppingListCheckOffService.buy;
+  }
+
+  AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtShoppingController(ShoppingListCheckOffService){
+    var controller = this;
+
+    controller.basket = ShoppingListCheckOffService.bought;
+  }
+
+  function ShoppingListCheckOffService(){
+    var service = this;
+
+    service.toBuy = [
+      { name: "cookies1", quantity: 1 },
+      { name: "cookies2", quantity: 2 },
+      { name: "cookies3", quantity: 3 },
+      { name: "cookies4", quantity: 4 },
+      { name: "cookies5", quantity: 5 }];
+
+    service.bought = [];
+
+    service.buy = function(index){
+      service.bought.push(service.toBuy[index]);
+      service.toBuy.splice(index, 1);
+    }
+
+    return service;
+  }
 })();
